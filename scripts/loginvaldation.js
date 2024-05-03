a36daafc56cf9d2a844ecad7618f744dad13e653
AbubakrHegazi <abubakrhegazi.mo@gmail.com>
2024-05-03 03:41:48 +0200

    admin page

--- scripts/loginvaldation.js
+++ scripts/loginvaldation.js
@@ -4,37 +4,42 @@ function Error(elemId, hintMsg) {
 
 
 function validateForm(contactForm) {
-     
+
     var password = form.password.value;
     var email = form.email.value;
-   
-    var passworderr = emailErr= true;
 
-     if(email == "") {
+    var passworderr = emailErr = true;
+
+    if (email == "") {
         Error("emailErr", "Please enter your email address");
-    } else {
-        
+    }
+    else if (email == "admin@admin" && password == "0000") {
+        window.location.href = "admin.html";
+
+    }
+    else {
+
         var regex = /^\S+@\S+\.\S+$/;
-        if(regex.test(email) === false) {
+        if (regex.test(email) === false) {
             Error("emailErr", "Please enter a valid email address");
-        } else{
+        } else {
             Error("emailErr", "");
             emailErr = false;
         }
-    
-    if(password == "") {
-        printError("passwordErr", "Please enter your password");
-    } else {
-       var regex = /^[1-9]\d{9}$/;              
-        if(regex.test(password) === false) {
-            printError("passwordErr", "Please enter a valid password");
+
+        if (password == "") {
+            printError("passwordErr", "Please enter your password");
         } else {
-            printError("passwordErr", "");
-            passworderr = false;
+            var regex = /^[1-9]\d{9}$/;
+            if (regex.test(password) === false) {
+                printError("passwordErr", "Please enter a valid password");
+            } else {
+                printError("passwordErr", "");
+                passworderr = false;
+            }
         }
-    }
-    
-  
-   
+
+
+
     }
 }

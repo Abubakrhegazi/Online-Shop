3971b1234cc340fc3a301771b4af6ceb6b1b8618
MennaKhaled17 <menakhalid2020@gmail.com>
2024-05-03 04:10:18 +0200

    fixed login

--- scripts/loginvaldation.js
+++ scripts/loginvaldation.js
@@ -1,45 +1,37 @@
-function Error(elemId, hintMsg) {
+function printError(elemId, hintMsg) {
     document.getElementById(elemId).innerHTML = hintMsg;
+    console.log('hello');
+
 }
 
 
-function validateForm(contactForm) {
 
-    var password = form.password.value;
-    var email = form.email.value;
+function validateForm() {
+    const form = document.getElementById('loginForm');
+    const email = form.email.value;
+    const password = form.password.value;
 
-    var passworderr = emailErr = true;
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
 
-    if (email == "") {
-        Error("emailErr", "Please enter your email address");
+    emailError.innerHTML = '';
+    passwordError.innerHTML = '';
+
+    if (email === '') {
+        emailError.innerHTML = 'Please enter your email address';
+        return false;
     }
-    else if (email == "admin@admin" && password == "0000") {
-        window.location.href = "admin.html";
 
+    if (password === '') {
+        passwordError.innerHTML = 'Please enter your password';
+        return false;
     }
-    else {
 
-        var regex = /^\S+@\S+\.\S+$/;
-        if (regex.test(email) === false) {
-            Error("emailErr", "Please enter a valid email address");
-        } else {
-            Error("emailErr", "");
-            emailErr = false;
-        }
-
-        if (password == "") {
-            printError("passwordErr", "Please enter your password");
-        } else {
-            var regex = /^[1-9]\d{9}$/;
-            if (regex.test(password) === false) {
-                printError("passwordErr", "Please enter a valid password");
-            } else {
-                printError("passwordErr", "");
-                passworderr = false;
-            }
-        }
-
-
-
+    if (email == 'admin@admin' && password == '0000') {
+        window.location.href = "admin.html"
     }
+
+    alert('Login successful');
+    form.reset();
+    return false;
 }

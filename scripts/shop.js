0624f1c0ed1604985946b6fa33f05955b2f14fd1
MennaKhaled17 <menakhalid2020@gmail.com>
2024-05-07 22:26:37 +0300

    editing

--- scripts/shop.js
+++ scripts/shop.js
@@ -45,7 +45,7 @@ function nextSlide() {
 
 function prevSlide() {
     let slide = document.getElementById("slide");
-    if(slideIndex == -1){
+    if(slideIndex == -1 ){
         slideIndex = images.length -1;
     }
     slide.src = images[slideIndex];

21a711076f6ce160b8fb40791157a060a2df44c7
AbubakrHegazi <abubakrhegazi.mo@gmail.com>
2024-05-08 00:20:26 +0200

    edits

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,9 +1,11 @@
 let products = [];
 function addProduct() {
-    let name = document.getElementById('name').value;
-    let price = document.getElementById('price').value;
-    let type = document.getElementById('type').value;
-    let image = document.getElementById('image').value;
+
+    let formm = document.getElementById('addProductForm');
+    let name = formm.name.value;
+    let price = formm.price.value;
+    let type = formm.type.value;
+    let image = formm.image.value;
 
     if (name === '') {
         document.getElementById('name-err').innerHTML = "Please fill name field";
@@ -33,5 +35,52 @@ function addProduct() {
         document.getElementById('error').innerHTML = "";
         let product = { name, price, type, image };
         products.push(product);
+        window.alert('Product added successfully!');
     }
+
 }
+
+
+function editProduct() {
+
+    let formm = document.getElementById('editProductForm');
+    let name = formm.name.value;
+    let price = formm.price.value;
+    let type = formm.type.value;
+    let image = formm.image.value;
+
+    if (name === '') {
+        formm.getElementById('name-err').innerHTML = "Please fill name field";
+    }
+    else {
+        formm.getElementById('name-err').innerHTML = "";
+    }
+    if (price === '') {
+        formm.getElementById('price-err').innerHTML = "Please fill in price field";
+    }
+    else {
+        formm.getElementById('price-err').innerHTML = "";
+    }
+    if (type === '') {
+        document.getElementById('type-err').innerHTML = "Please fill in type field";
+    }
+    else {
+        document.getElementById('type-err').innerHTML = "";
+    }
+    if (image === '') {
+        document.getElementById('image-err').innerHTML = "Please select an image";
+    }
+    else {
+        document.getElementById('image-err').innerHTML = "";
+    }
+    if (!(name === '' || price === '' || type === '' || image === '')) {
+        document.getElementById('error').innerHTML = "";
+        let product = { name, price, type, image };
+        products.push(product);
+        window.alert('Product added successfully!');
+    }
+
+}
+
+
+

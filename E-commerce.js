0c7f89112bbaac5de47002e33fec140266dea2df
Ali <aliabdalla2110@gmail.com>
2024-05-08 10:16:39 +0300

    home page design

--- E-commerce.js
+++ E-commerce.js
@@ -44,24 +44,3 @@ cartIcons.forEach(cartIcon => {
     });
 });
 
-const toggle = document.getElementById('toggle-dark');
-const body = document.querySelector('body');
-
-function toggleDarkMode() {
-    body.classList.toggle('dark-mode');
-    const isDarkMode = body.classList.contains('dark-mode');
-    localStorage.setItem('darkMode', isDarkMode);
-}
-
-toggle.addEventListener('click', function(){
-    toggleDarkMode();
-});
-
-window.onload = function() {
-    const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
-    if (darkModeEnabled) {
-        body.classList.add('dark-mode');
-    }
-};
-
-

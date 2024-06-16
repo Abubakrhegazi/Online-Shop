18b5ac9d8fb3d002a517e839062d8938411e0bb8
AbubakrHegazi <abubakrhegazi.mo@gmail.com>
2024-06-16 19:37:03 +0200

    rr

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,12 +1,14 @@
-const express=require('express')
+const express = require('express')
 require("dotenv").config()
+const Product = require('../models/product');
 
 module.exports = {
     home_get: (req, res) => {
         res.render('index', { title: 'Home', currentPage: 'home' });
     },
-    shop_get: (req, res) => {
-        res.render('shop', { title: 'Shop', currentPage: 'shop' });
+    shop_get: async(req, res) => {
+        const data = await Product.find().sort({ createdAt: -1 });
+        res.render('shop', { title: 'Shop', currentPage: 'shop',data:data});
     },
     about_get: (req, res) => {
         res.render('about', { title: 'About', currentPage: 'about' });

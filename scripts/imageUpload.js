4976dc14971d761d50c771d5fc5df15000f1d3e0
MennaKhaled17 <menakhalid2020@gmail.com>
2024-06-19 00:07:51 +0300

    image p

---
+++ scripts/imageUpload.js
@@ -0,0 +1,11 @@
+function imageupload(){
+    <div classname="auth-wrapper">
+        <div classname="auth-inner" style={{width:"auto"}}>
+           < input accept="image/*" type="file" >
+           
+           </input>
+    </div>
+
+    </div>
+
+}
